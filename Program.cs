11953815ba2059f8cbe55c2fd7cf3584dc84505c
Cristian Malaga <cmalaga@usf.edu>
2019-09-12 00:24:57 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace deliverable2_IterativeStatments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a value between 1 and 100 ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (input >= 1 && input <= 100)
+            {
+                for (int iteration = 0; iteration <= input; iteration++)
+                {
+                    Console.WriteLine("You have entered " + input +"." + " This is the current value in the loop : " + iteration);
+                }
+            }
+        }
+    }
+}
